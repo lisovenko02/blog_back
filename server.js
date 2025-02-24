18f4483f70c0b66741f8e3e7d750a20b48167993
lisovenko02 <vanyalisovenkoo@gmail.com>
2025-02-24 16:56:14 +0200

    fix some bugs

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app.js"
+import mongoose from 'mongoose'
+import app from './app.js'
 
-const {DB_HOST} = process.env;
+const { DB_HOST } = process.env
 
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true)
 
-mongoose.connect(DB_HOST)
-    .then(() => {
-        app.listen(4000);
-        console.log("Database connection successful")
-    })
-    .catch((error) => {
-        process.exit(1);
-    })
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(4000)
+    console.log('Database connection successful')
+  })
+  .catch((error) => {
+    process.exit(1)
+  })
